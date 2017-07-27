254ee877c3bf563e7d620ffe20b0e132d19ca649
DmitriyK <daemon700@ukr.net>
2017-07-27 15:48:24 +0000

    Added plan vars and params to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+    #GET request for /nothing and this nothing is our home page!
     def home
+        @basic_plan= Contact.find(1)
+        @pro_plan= Contact.find(2)
     end 
     def about
     end end
