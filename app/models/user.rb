b6752d7ea22324b8312816340cef573069e03950
DmitriyK <daemon700@ukr.net>
2017-07-31 16:08:47 +0000

    Added server side code for creating subscriptions for pro users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+         
          belongs_to :plan
+
+
+attr_accessor :stripe_card_token
+def save_with_subscription
+  if valid?
+    customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+    self.stripe_customer_token = customer.id
+    save!
+  end
+end
 end
